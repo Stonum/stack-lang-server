e05b89c5d407015aea4c8d341b9f50a2368d2aed
Sychev Roman <53710482+ethkinkod@users.noreply.github.com>
2026-03-26 08:38:45 +0300

    feat(mlang): completions for class static variables (#140)
    * added class variables lazzy analizer
    
    * filter unique completion labels and refactor
    
    * changed to Property for MClassMethodType
    
    * parsing variables in fn class_definition
    
    * refactor class_definition
    
    * add prop ranges
    
    * add tests and name fix
    
    ---------
    
    Co-authored-by: Сычёв Роман Сергеевич <sychev_rs@stack.stack-it.ru>

--- mlang_semantic/src/declaration.rs
+++ mlang_semantic/src/declaration.rs
@@ -9,8 +9,8 @@ use mlang_lsp_definition::{
 };
 use mlang_syntax::{
     AnyMClassMember, AnyMFunction, AnyMLiteralExpression, AnyMParameterList, AnyMSwitchClause,
-    MClassDeclaration, MFileSource, MFunctionDeclaration, MLanguage, MReport, MReportSection,
-    MSyntaxNode,
+    MClassDeclaration, MFileSource, MFunctionDeclaration, MLanguage, MName, MReport,
+    MReportSection, MSyntaxNode,
 };
 
 use crate::SemanticModel;
@@ -72,6 +72,15 @@ impl CodeSymbolDefinition for AnyMDefinition {
         match self {
             AnyMDefinition::MClassMemberDefinition(member) => {
                 member.m_type == MClassMethodType::Setter
+            }
+            _ => false,
+        }
+    }
+
+    fn is_property(&self) -> bool {
+        match self {
+            AnyMDefinition::MClassMemberDefinition(member) => {
+                member.m_type == MClassMethodType::Property
             }
             _ => false,
         }
@@ -234,6 +243,7 @@ impl CodeSymbolInformation for AnyMDefinition {
                 MClassMethodType::Getter => SymbolKind::PROPERTY,
                 MClassMethodType::Setter => SymbolKind::PROPERTY,
                 MClassMethodType::Method => SymbolKind::METHOD,
+                MClassMethodType::Property => SymbolKind::VARIABLE,
             },
             AnyMDefinition::MReportDefinition(_) => SymbolKind::CONSTANT,
             AnyMDefinition::MReportSectionDefiniton(_) => SymbolKind::FIELD,
@@ -433,6 +443,7 @@ enum MClassMethodType {
     Constructor,
     Getter,
     Setter,
+    Property,
     #[default]
     Method,
 }
@@ -518,16 +529,16 @@ pub(crate) fn prepare_definitions(
     }
 
     if let Some(class) = MClassDeclaration::cast(node.clone())
-        && let Some((class, metohds)) = class_definition(class, index)
+        && let Some((class, members)) = class_definition(class, index)
     {
-        let mut metohds = metohds
+        let mut members: Vec<AnyMDefinition> = members
             .into_iter()
             .map(AnyMDefinition::MClassMemberDefinition)
             .collect();
         model
             .definitions
             .push(AnyMDefinition::MClassDefinition(class));
-        model.definitions.append(&mut metohds);
+        model.definitions.append(&mut members);
     }
 
     if let Some(report) = MReport::cast(node.clone())
@@ -683,13 +694,15 @@ fn class_definition(
             .and_then(|ext| Some(ext.super_class().ok()?.text())),
     });
 
-    let methods = class
+    let mut members = class
         .members()
         .iter()
         .filter_map(|member| class_member_definition(member, Arc::downgrade(&class_def), index))
         .collect::<Vec<_>>();
 
-    Some((class_def, methods))
+    add_props_to_class_members(&mut members, class, index, &class_def);
+
+    Some((class_def, members))
 }
 
 fn class_member_definition(
@@ -725,6 +738,84 @@ fn class_member_definition(
             AnyMClassMember::MSetterClassMember(_) => MClassMethodType::Setter,
             _ => MClassMethodType::Method,
         },
+    })
+}
+
+// add all class props that are not explicitly declared to members
+// like
+// class Test {
+//    constructor() { this.a = 1; }
+// }
+// where 'a' is class variable
+fn add_props_to_class_members(
+    members: &mut Vec<MClassMemberDefinition>,
+    class: MClassDeclaration,
+    index: &LineIndex,
+    class_def: &Arc<MClassDefinition>,
+) {
+    let constructor = class
+        .members()
+        .iter()
+        .filter_map(|m| m.as_m_constructor_class_member().cloned())
+        .next();
+
+    if let Some(constructor) = constructor
+        && let Some(body) = constructor.body().ok()
+    {
+        let mut static_members = body
+            .statements()
+            .iter()
+            .filter_map(|s| {
+                let expr_stmt = s.as_m_expression_statement()?;
+                let expr = expr_stmt.expression().ok()?;
+                let assign = expr.as_m_assignment_expression()?;
+                let left = assign.left().ok()?;
+                let static_member = left.as_m_static_member_assignment()?;
+                static_member.member().ok().iter().next().cloned()
+            })
+            .collect::<Vec<_>>();
+        static_members.dedup_by_key(|m| m.text().to_lowercase());
+
+        let mut variables = static_members
+            .iter()
+            .filter(|variable_name| {
+                !members.iter().any(|m| {
+                    m.id.name
+                        .to_lowercase()
+                        .eq(variable_name.text().to_lowercase().as_str())
+                })
+            })
+            .filter_map(|variable_name| {
+                class_property_definition(variable_name, Arc::downgrade(class_def), index)
+            })
+            .collect();
+
+        members.append(&mut variables);
+    }
+}
+
+fn class_property_definition(
+    variable_name: &MName,
+    class: Weak<MClassDefinition>,
+    index: &LineIndex,
+) -> Option<MClassMemberDefinition> {
+    let member_range = index.line_col_range(variable_name.range())?;
+    Some(MClassMemberDefinition {
+        keyword: None,
+        id: DefinitionId {
+            name: variable_name.text().to_string(),
+            range: member_range,
+        },
+        class,
+        params: MParameters {
+            text: String::from(""),
+            total_count: 0,
+            optional_count: 0,
+            has_rest: false,
+        },
+        description: None,
+        range: member_range,
+        m_type: MClassMethodType::Property,
     })
 }
 
@@ -998,6 +1089,135 @@ mod tests {
                 description: None,
                 range: line_col_range(21, 8, 23, 9),
                 m_type: MClassMethodType::Method
+            })
+        );
+    }
+
+    #[test]
+    fn test_convert_class_props_to_definitions() {
+        let text = r#"
+    class x {
+        constructor() {
+            this._x = 0;
+            this.x = 1;
+        }
+
+        get x() {
+            return this._x;
+        }
+
+        set x( val ) {
+            this._x = val;
+        }
+    }
+    "#;
+        let file_source = MFileSource::module();
+        let parsed = parse(text, file_source);
+
+        let semantic_model = semantics(text, parsed.syntax(), file_source);
+        let mut definitions = semantic_model.definitions();
+
+        println!("{:?}", definitions.len());
+
+        assert_eq!(definitions.len(), 5);
+
+        assert_eq!(
+            *definitions.next().unwrap(),
+            AnyMDefinition::MClassDefinition(Arc::new(MClassDefinition {
+                keyword: String::from("class"),
+                id: DefinitionId {
+                    name: String::from("x"),
+                    range: line_col_range(1, 10, 1, 11)
+                },
+
+                description: None,
+                range: line_col_range(1, 4, 14, 5),
+                extends: None,
+                methods: vec![]
+            }))
+        );
+
+        assert_eq!(
+            *definitions.next().unwrap(),
+            AnyMDefinition::MClassMemberDefinition(MClassMemberDefinition {
+                keyword: None,
+                id: DefinitionId {
+                    name: String::from("constructor"),
+                    range: line_col_range(2, 8, 2, 19)
+                },
+                class: Weak::new(),
+                params: MParameters {
+                    text: String::from("()"),
+                    total_count: 0,
+                    optional_count: 0,
+                    has_rest: false
+                },
+                description: None,
+                range: line_col_range(2, 8, 5, 9),
+                m_type: MClassMethodType::Constructor
+            }),
+        );
+
+        assert_eq!(
+            *definitions.next().unwrap(),
+            AnyMDefinition::MClassMemberDefinition(MClassMemberDefinition {
+                keyword: None,
+                id: DefinitionId {
+                    name: String::from("x"),
+                    range: line_col_range(7, 12, 7, 13)
+                },
+                class: Weak::new(),
+                params: MParameters {
+                    text: String::from(""),
+                    total_count: 0,
+                    optional_count: 0,
+                    has_rest: false
+                },
+                description: None,
+                range: line_col_range(7, 8, 9, 9),
+                m_type: MClassMethodType::Getter
+            }),
+        );
+
+        assert_eq!(
+            *definitions.next().unwrap(),
+            AnyMDefinition::MClassMemberDefinition(MClassMemberDefinition {
+                keyword: None,
+                id: DefinitionId {
+                    name: String::from("x"),
+                    range: line_col_range(11, 12, 11, 13)
+                },
+                class: Weak::new(),
+                params: MParameters {
+                    text: String::from("( val )"),
+                    total_count: 1,
+                    optional_count: 0,
+                    has_rest: false
+                },
+                description: None,
+                range: line_col_range(11, 8, 13, 9),
+                m_type: MClassMethodType::Setter
+            }),
+        );
+
+        assert_eq!(
+            *definitions.next().unwrap(),
+            AnyMDefinition::MClassMemberDefinition(MClassMemberDefinition {
+                keyword: None,
+                id: DefinitionId {
+                    name: String::from("_x"),
+                    range: line_col_range(3, 17, 3, 19)
+                },
+                class: Weak::new(),
+                params: MParameters {
+                    text: String::from(""),
+                    total_count: 0,
+                    optional_count: 0,
+                    has_rest: false
+                },
+                description: None,
+                range: line_col_range(3, 17, 3, 19),
+                m_type: MClassMethodType::Property
             })
         );
     }
