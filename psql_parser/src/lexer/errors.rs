739676906f1520f40ba1177dc9cbb45b39df1310
stonum <lexxarsh@gmail.com>
2026-07-16 15:50:54 +0300

    chore(psql): remove unused escape-sequence diagnostic helper

--- psql_parser/src/lexer/errors.rs
+++
@@ -1,6 +0,0 @@
-use biome_parser::prelude::*;
-
-pub fn invalid_digits_after_escape_sequence(start: usize, end: usize) -> ParseDiagnostic {
-    ParseDiagnostic::new("invalid digits after escape sequence", start..end)
-        .with_hint("expected valid escape sequence")
-}
