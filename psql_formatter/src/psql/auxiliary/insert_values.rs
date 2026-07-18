8c787d6a62316948afeee05cd9d7dc9e9af8bd4a
stonum <lexxarsh@gmail.com>
2026-07-18 21:41:53 +0300

    feat(psql): format INSERT statements (values/select, on conflict, returning)

--- psql_formatter/src/psql/auxiliary/insert_values.rs
+++ psql_formatter/src/psql/auxiliary/insert_values.rs
@@ -1,10 +1,27 @@
 use crate::prelude::*;
-use biome_rowan::AstNode;
+use biome_formatter::write;
 use psql_syntax::PsqlInsertValues;
+use psql_syntax::PsqlInsertValuesFields;
 #[derive(Debug, Clone, Default)]
 pub(crate) struct FormatPsqlInsertValues;
 impl FormatNodeRule<PsqlInsertValues> for FormatPsqlInsertValues {
     fn fmt_fields(&self, node: &PsqlInsertValues, f: &mut PsqlFormatter) -> FormatResult<()> {
-        format_verbatim_node(node.syntax()).fmt(f)
+        let PsqlInsertValuesFields {
+            values_token,
+            l_paren_token,
+            items,
+            r_paren_token,
+        } = node.as_fields();
+
+        write!(
+            f,
+            [
+                values_token.format(),
+                space(),
+                l_paren_token.format(),
+                group(&soft_block_indent(&items.format())),
+                r_paren_token.format(),
+            ]
+        )
     }
 }
--- psql_formatter/src/psql/clauses/set_clause.rs
+++ psql_formatter/src/psql/clauses/set_clause.rs
@@ -1,10 +1,19 @@
 use crate::prelude::*;
-use biome_rowan::AstNode;
+use biome_formatter::{format_args, write};
 use psql_syntax::PsqlSetClause;
+use psql_syntax::PsqlSetClauseFields;
 #[derive(Debug, Clone, Default)]
 pub(crate) struct FormatPsqlSetClause;
 impl FormatNodeRule<PsqlSetClause> for FormatPsqlSetClause {
     fn fmt_fields(&self, node: &PsqlSetClause, f: &mut PsqlFormatter) -> FormatResult<()> {
-        format_verbatim_node(node.syntax()).fmt(f)
+        let PsqlSetClauseFields { set_token, items } = node.as_fields();
+
+        write!(
+            f,
+            [group(&format_args![
+                set_token.format(),
+                soft_line_indent_or_space(&items.format())
+            ])]
+        )
     }
 }
